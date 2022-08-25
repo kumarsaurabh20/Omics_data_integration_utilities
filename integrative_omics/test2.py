6f18b413870a189c40805a6557001fd5e506d03a
Kumar Saurabh Singh <kumarsaurabh20@gmail.com>
2022-08-25 13:48:39 +0200

    script

--- integrative_omics/test2.py
+++ integrative_omics/test2.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 
 #con = sqlite3.connect("mvc.db")
-con = sqlite3.connect("NPDB_merge.sqlite")
+con = sqlite3.connect("/Users/singh018/Documents/NPDB_merge.sqlite")
 cur = con.cursor()
 
 #sql = "SELECT uniprot_clusters.uniprot_id, rules.reaction_id " \
@@ -13,7 +13,7 @@ cur = con.cursor()
 #      "INNER JOIN smiles_clusters ON smiles_clusters.cluster_id = uniprot_clusters.cluster_id " \
 #      "INNER JOIN rules ON rules.smiles_id = smiles_clusters.smiles_id;"
 
-sql = "SELECT * FROM correlations_HB;"
+sql = "SELECT ms_name,mz, monoisotopic_mass, smile, inchi_key2 FROM merged_data;"
 cursor = cur.execute(sql)
 temp = []
 for each in cursor:
@@ -22,5 +22,13 @@ for each in cursor:
 
 con.close()
 uniprot_ids = pd.DataFrame(temp)
-uniprot_ids.to_csv("HB_correlations.tsv", sep="\t")
+uniprot_ids.columns = ["ms_name", "mz", "mm", "smile", "inchi_key2"]
+print("Original count of the dataframe without filtering {}".format(uniprot_ids.shape))
+final = uniprot_ids.drop_duplicates(subset=['inchi_key2'], keep="first")
+print("Shape of the dataframe with inchi_key2 as filtering criteria {}".format(final.shape))
+final2 = uniprot_ids.drop_duplicates(subset=['smile'], keep="first")
+print("Shape of the dataframe with smile as filtering criteria {}".format(final2.shape))
+new_masses = final2[['ms_name', 'mz', 'mm']]
+new_masses.to_csv("smile_queryMassNPDB.csv", sep=",", index=False)
 
+#uniprot_ids.to_csv("queryMassNPDB_hp.csv", sep=",", index=False)
